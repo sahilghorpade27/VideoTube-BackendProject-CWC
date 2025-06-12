930c04bb165497b4eb3bf630bc6f3ad9f8d5ff51
Sahil Ghorpade <sahilpg27@gmail.com>
2025-06-12 10:42:57 +0530

    fixing the routes - but not success

--- src/app.js
+++ src/app.js
@@ -2,25 +2,24 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import express from "express";
-
-
 const app = express();
+
 app.use(cors({
     origin: process.env.CORS_ORIGIN ,
     credentials : true 
 }));
-
-app.use(express.json({
-    limit : "16kb"
-}));
-
-app.use(express.urlencoded({
-    limit : "16kb",
-    extended : true 
-}));
-
+app.use(express.json({limit : "16kb"}));
+app.use(express.urlencoded({limit : "16kb",extended : true}));
 app.use(express.static("public"));
-
 app.use(cookieParser());
+
+//importing routes 
+
+import userRouter from "./routes/user.routes.js" ;
+
+//routes declaration
+
+app.use("/api/v1/users" , userRouter);
+//http://localhost:8000/api/v1/users/register
 
 export {app}

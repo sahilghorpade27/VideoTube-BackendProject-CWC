6cb61d17707f29628a2792120b62af21c0c131c3
Sahil Ghorpade <sahilpg27@gmail.com>
2025-06-17 00:41:54 +0530

    Tested the register controller using parameteres in postman

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -18,7 +18,7 @@ const registerUser = asyncHandler(async(req,res)=> {
     }*/
 
     if(
-        [fulName , email , username , password ].some((field) => 
+        [fullName , email , username , password ].some((field) => 
             field?.trim() === "") 
     ){
         throw new ApiError(400, "All fields are required");
@@ -26,7 +26,7 @@ const registerUser = asyncHandler(async(req,res)=> {
 
     //check if user exists 
 
-    const existedUser =  User.findOne({
+    const existedUser =  await User.findOne({
         $or : [{email},{username}]
     })
 
@@ -34,12 +34,19 @@ const registerUser = asyncHandler(async(req,res)=> {
         throw new ApiError(409, "User with email or Username exists");
     }
 
+    // console.log(req.files);
+
     
 
     // check for images and  avatar
 
     const avatarLocalPath = req.files?.avatar[0]?.path
-    const coverImageLocalPath = req.files?.coverImage[0]?.path
+    //const coverImageLocalPath = req.files?.coverImage[0]?.path
+
+    let coverImageLocalPath ;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
 
     if(!avatarLocalPath){
         throw new ApiError(400, "Avatar file is required")
@@ -64,13 +71,13 @@ const registerUser = asyncHandler(async(req,res)=> {
         coverImage : coverImage?.url|| "" ,
         email ,
         password ,
-        username : username.toLowercase()
+        username : username.toLowerCase()
 
     })
 
     // remove password and refresh token field from response 
 
-    const createdUser = await User.findById(user.email._id).select(
+    const createdUser = await User.findById(user._id).select(
         "-password -refreshToken"
     )
 
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
-import {upload} from "../middlwares/multer.middlware.js";
+import {upload} from "../middlewares/multer.middleware.js";
 
 const router = Router()
 
 router.route("/register").post(
     upload.fields([
         {
-            name : "avatar ",
+            name : "avatar",
             maxCount : 1
         },
         {
